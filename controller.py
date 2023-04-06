63d993f57f38b2dd26a8aeeb380650e7c37d32e5
Eli Newton <enewton@monmouthcollege.edu>
2023-04-06 08:04:59 -0500

    Added more to view.

--- controller.py
+++ controller.py
@@ -1,7 +1,14 @@
+import pygame
+from pygame.math import Vector2
+from model import Model
+from view import GoLView
+
 class Controller:
     def __init__(self,model,view):
         self.model = model
         self.view = view
 
-
-    
+    #def cellChange(self, event):
+        #if event.type == pygame.MOUSEBUTTONDOWN:
+         #   self._DS.fill((255,255,0))
+            
